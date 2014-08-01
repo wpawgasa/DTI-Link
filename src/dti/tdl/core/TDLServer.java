6eb8ddf43d84c4010404466a181c4b24c0f23f1d
wpawgasa <wpawgasa@gmail.com>
2014-08-01 09:32:07 +0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/dti/tdl/core/TDLInterface.java

--- src/dti/tdl/core/TDLServer.java
+++ src/dti/tdl/core/TDLServer.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -68,7 +70,8 @@ public class TDLServer extends Thread {
                 thread.setName("TDL Data Service");
                 thread.start();
             } catch (Exception e) {
-
+                e.printStackTrace();
+                Logger.getLogger(TDLServer.class.getName()).log(Level.SEVERE, null, e);
             }
         }
     }
